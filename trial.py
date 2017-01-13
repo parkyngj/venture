f3673e1d2a42bc5c48960153378a1d892e5eceec
Young Ju Park <parkyngj@gmail.com>
2017-01-12 18:44:25 -0800

    adds lists and tuples to trials

--- trial.py
+++ trial.py
@@ -109,3 +109,118 @@ bool("")    # => False
 bool([])    # => False
 bool({})    # => False
 bool(())    # => False
+
+################################
+# 2. Variables and Collections #
+################################
+
+# Python has a print function
+print("I'm Python. Nice to meet you!") # => I'm Python. Nice to meet you!
+
+# By default, the print function also prints out a newline at the end.
+# Use the optional argument end to change the end character
+print("Hello World", end="!") # => Hello, World!
+
+# Simple way to get input data from console:
+input_string_var = input("Enter some data: ") # Returns the data as a string
+
+# Declare variables by simple assignment
+some_var = 5
+some_var # => 5
+
+# Trying to access a previously unassigned variable is an exception
+some_unassigned_var # Raises a NameError
+
+# If can be used as an expression
+"yahoo!" if 3 > 2 else 2 # => "yahoo!"
+
+# Lists store sequences
+li = []
+
+# You can start with prepopulated list
+other_lit = [1,2,3]
+
+# Add stuff to the end of a list with append
+li.append(1)    # li is now [1]
+li.append(2)    # li is now [1,2]
+li.append(4)    # li is now [1,2,4]
+li.append(3)    # li is now [1,2,4,3]
+# Remove from the end of the list with pop
+li.pop()        # => 3, and li is now [1,2,4]
+# Let's put 3 back
+li.append(3)    # li is now [1,2,4,3] again
+
+# Access list elements by nindex
+li[0] # => 1
+# Negative indexing works
+li[-1] # => 3
+
+# Looking out of bounds is an IndexError
+li[4] # => Raises an IndexError
+
+# Can look at ranges with slice syntax
+# (It is a closed/open range)
+li[1:3]     # => [2,4]
+# Omit up to and including some index N:
+li[2:]      # => [4,3]
+# Slice off after and including some index N:
+li[:3]      # => [1,2,4]
+# Select every second entry
+li[::2]     # => [1,4]
+# Return a reversed copy of the list
+li[::-1]    # => [3,4,2,1]
+# Use any combinato of these to make advanced slices
+# the key is li[start:end:step]
+
+# Make a one layer deep copy using slices:
+li2 = li[:] # => li2 = [1,2,4,3], but (li2 is li) will result in False
+
+# Remove an item at index N with "del"
+del li[2]       # li is now [1,2,3]
+
+# Remove first occurrence of a value with .remove
+li.remove(2)    # li is now [1,3]
+li.remove(2)    # Raises a ValueError as 2 is not in the list
+
+# Insert an element at a specific index
+li.insert(1,2)  # li is now [1,2,3] again
+# the format is li.insert(INDEX, VALUE)
+
+# Get the index of the first item found matching the argument
+li.index(2)     # => 1
+li.index(4)     # => Raises a ValueError
+
+# You can concatenate lists together with + (not destructive to any lists used in the concatenation)
+li + other_li           # => [1,2,3,4,5,6]
+# .extend is the destructive version of +
+li.extend(other_li)     # => Now li is [1,2,3,4,5,6]
+
+# Check for existence of an element in a list with "in"
+1 in li # => True
+
+# Return the length of a list with "len()"
+len(li) # => 6
+
+# Tuples are like lists, but immutable
+tup = (1,2,3)
+tup[0]      # => 1
+tup[0] = 3  # Raises a TypeError
+
+# Note: Tuple of length one has to have comma after the last element, but tuples of other lengths, even zero, don't
+type((1))   # => <class 'int'>
+type((1,))  # => <class 'tuple'>
+type(())    # => <class 'tuple'>
+
+# You can do most of the list operations on tuples too, like asking length, concatenation, selection, inclusion
+len(tup)        # => 3
+tup + (4,5,6)   # => (1,2,3,4,5,6)
+tup[:2]         # => (1,2)
+2 in tup        # => True
+
+# Unpack tuples or lists into respective variables
+a,b,c = (1,2,3)         # a is now 1, b is now 2, and c is now 3
+d,e,f = 4,5,6           # d is now 4, e is now 5, and f is now 6
+# Can do extended unpacking
+a, *b, c = (1,2,3,4)    # a is now 1, b is now [2,3], and c is now 4
+# Can swap two values as such
+d, e = e, d             # now d is 5 and e is 4
