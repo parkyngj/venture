232f4d81a7d75f897298b33280ee55e14e876c60
Young Ju Park <parkyngj@gmail.com>
2017-01-12 19:20:55 -0800

    adds sets to trials

--- trial.py
+++ trial.py
@@ -268,4 +268,40 @@ filled_dict.update({"four": 4}) # => {"one": 1, "two": 2, "three": 3, "four": 4}
 # Remove keys from a dictionary with del
 del filled_dict['one'] # Removes the key "one" from filled dict
 
-# 
+# Sets store sets (no duplicates, fast lookup for inclusion)
+empty_set = set()
+# Initialize a set with some values:
+some_set = {1, 1, 2, 2, 3, 4} # some_set is {1,2,3,4} (note that the duplicates 1 and 2 have been collapsed)
+
+# Similar to keys of dictionary, elements of a set must be immutable
+invalid_set = {[1], 1} # => Raises a TypeError: unhashable type: 'list'
+valid_set = {(1,), 1}
+
+# Can assign new variables to a set
+filled_set = some_set
+
+# Add one more item to the set
+filled_set.add(5) # filled_set is now {1,2,3,4,5}
+
+# Can do set intersection with &
+other_set = {3,4,5,6}
+filled_set & other_set # => {3,4,5}
+
+# Can do set union with |
+filled_set | other_set # => {1,2,3,4,5,6}
+
+# Do set difference with -
+{1, 2, 3, 4} - {2, 3, 5}  # => {1, 4}
+
+# Do set symmetric difference with ^
+{1, 2, 3, 4} ^ {2, 3, 5}  # => {1, 4, 5}
+
+# Check if set on the left is a superset of set on the right
+{1, 2} >= {1, 2, 3} # => False
+
+# Check if set on the left is a subset of set on the right
+{1, 2} <= {1, 2, 3} # => True
+
+# Check for existence in a set with in
+2 in filled_set   # => True
+10 in filled_set  # => False
