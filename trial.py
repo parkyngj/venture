5ed00ce6bc6a2a696bb3197ce393921d4316b431
Young Ju Park <parkyngj@gmail.com>
2017-01-12 19:03:54 -0800

    adds dictionaries to trials

--- trial.py
+++ trial.py
@@ -224,3 +224,48 @@ d,e,f = 4,5,6           # d is now 4, e is now 5, and f is now 6
 a, *b, c = (1,2,3,4)    # a is now 1, b is now [2,3], and c is now 4
 # Can swap two values as such
 d, e = e, d             # now d is 5 and e is 4
+
+# Note keys for dictionary must be immutable types.
+    # This is to ensure that the key can be converted into a constant hash value for quick look-ups.
+    # Immutable types include integers, floats, strings, and tuples
+invalid_dict    = {[1,2,3]: "123"}      # => Raises a TypeError: unhashable type: 'list'
+valid_dict       = {(1,2,3): [1,2,3]}    #=> Values can be of any type, however
+
+# We can look up values with []
+filled_dict["one"] # => 1
+
+# Get all keys as an iterable with "keys()", and wrap it in list() to turn it into a list.
+# Note: Dictionary key ordering isn't guaranteed. Our results may not match this exactly.
+list(filled_dict.keys()) #=> ["three", "two", "one"]
+
+# Get all values as an iterable with "values()". Once again we need to wrap it
+# in list() to get it out of the iterable. Note - Same as above regarding key
+# ordering.
+list(filled_dict.values())  # => [3, 2, 1]
+
+# Check for existence of keys in a dictionary with "in"
+"one" in filled_dict    # => True
+1 ni filled_dict        # => False
+
+# Looking up a non-existing key is a KeyError
+filled_dict["four"] # KeyError
+
+# Use "get()" method to avoid the KeyError
+filled_dict.get("one")  # => 1
+filled_dict.get("four") # => None
+# "get()" method supports a default argument when the value is missing
+filled_dict.get('one', 4)   # => 1
+filled_dict.get('four', 4)  # => 4
+
+# "setdefault()" inserts into a dictionary only if the given key isn't present
+filled_dict.setdefault("five", 5)   # filled_dict['five'] is set to 5
+filled_dict.setdefault("five", 6)   # filled_dict['five'] is still 5
+
+# Adding to a dictionary
+filled_dict.update({"four": 4}) # => {"one": 1, "two": 2, "three": 3, "four": 4}
+# filled_dict["four"] = 4       # another way to add to dictionary
+
+# Remove keys from a dictionary with del
+del filled_dict['one'] # Removes the key "one" from filled dict
+
+# 
